eaa3db4df991894199d8f78dffe06a9b0381d05c
garvit74 <63715496+garvit74@users.noreply.github.com>
2022-11-15 03:30:38 +0530

    Time: 174 ms (48.33%), Space: 48.4 MB (46.33%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,20 +1,9 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, -1);
-        return f(0, n, nums, dp);
-    }
-	
-	bool f(int ind, int n, vector<int>& nums, vector<int>& dp) {
-        if(ind == n-1) return true;
-        if(dp[ind] != -1) return dp[ind];
-        
-        int jump = nums[ind];
-        bool check = false;
-        for(int i = ind+1; i <= min(n-1, ind+jump); i++)
-            check = check || f(i, n, nums, dp);
-        
-        return dp[ind] = check;
+        int i = 0, n = nums.size();
+        for(int reach = 0; i < n && i <= reach; i++)
+            reach = max(reach, nums[i] + i);
+        return i == n;
     }
 };
